adf584b73b0ebd5c47ef21a5c00ed85efeb17f71
Scott Maclure <scott@maclure.com.au>
2017-03-12 19:34:57 +1100

    First implementation of progress bars API in go, using Gin.

---
+++ api.go
@@ -0,0 +1,37 @@
+package main
+
+import "math/rand"
+import "github.com/gin-gonic/gin"
+
+func randInt(min, max int) int {
+	return (min + rand.Intn((max+1)-min))
+}
+
+func bars() []int {
+	size := randInt(2, 5)
+	data := []int{}
+	for i := 0; i < size; i++ {
+		data = append(data, randInt(10, 90))
+	}
+	return data
+}
+
+func buttons() []int {
+	return []int{
+		randInt(5, 50), randInt(5, 50),
+		randInt(-50, -5), randInt(-50, -5),
+	}
+}
+
+func barsPayload(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"buttons": buttons(),
+		"bars":    bars(),
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/bars", barsPayload)
+	r.Run() // listen and serve on 8080
+}
